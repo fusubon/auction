18a7ae718eff99323b9a30fcc9b2a21ff4e78c11
fusubon <info@fusubon.com>
2019-07-13 14:24:22 +0900

    照会画面完成

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,9 @@
+class ItemsController < ApplicationController
+
+def show
+    # Itemモデルのfindメソッドを使用して、パラメータの値に一致するidのデータを検索している
+    # 取得したデータを@itemというインスタンス変数にセットする @item = Item.find(params[:id])
+    @item = Item.find(params[:id])
+end
+
+end
