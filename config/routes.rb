01555e46f1e5de7ab5b626ebb7211f2982fa4028
fusubon <info@fusubon.com>
2019-07-13 10:17:13 +0900

    First message app complete!

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get(HTTPメソッド) 'リクエストしたURL' => 'コントローラ名#アクション名'
+  # 下記の1行を追加(do 〜 endの中に記述して下さい)
+  get 'messages/hello' => 'messages#hello'
 end
