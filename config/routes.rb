ead36c22d82138728e1d92557aca79051c89e403
fusubon <info@fusubon.com>
2019-07-13 13:51:48 +0900

    ルーティング確認

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources リソース名(複数のitem(商品)を取り扱うのでitemsとなります。
+  resources :items
+
 end
